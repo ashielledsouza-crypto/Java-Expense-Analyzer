4dd609d5192759b19de79fba865338a0aa8358a3
ashielledsouza-crypto <ashielle.dsouza@gmail.com>
2026-09-03 19:39:54 +0530

    Fixed income vs expense

--- src/main/java/com/expensetracker/expense_analyzer/expense/config/WebConfig.java
+++ src/main/java/com/expensetracker/expense_analyzer/expense/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Apply this rule to all your API endpoints
-                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:5500") // Add your frontend URLs here
+                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:5500","http://localhost:8080") // Add your frontend URLs here
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
